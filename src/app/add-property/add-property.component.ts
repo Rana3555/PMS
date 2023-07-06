260e1e3c9f53403658259a50d1ad53543c9240ec
charan <charan.chunchu@peopletech.com>
2023-07-06 16:28:52 +0530

    chips and dialog corrcetions

--- src/app/add-property/add-property.component.ts
+++ src/app/add-property/add-property.component.ts
@@ -13,20 +13,6 @@ export interface Place {
 export class AddPropertyComponent {
 
   profileImage: string | null = null;
-
-// onFileSelected(event: Event) {
-//     const inputElement = event.target as HTMLInputElement;
-//     const files = (inputElement.files as FileList);
-//     if (files && files.length > 0) {
-//       const file = files[0];
-//       const reader = new FileReader();
-//       reader.onload = (event: ProgressEvent<FileReader>) => {
-//         this.profileImage = event.target?.result as string;
-//       };
-//       reader.readAsDataURL(file);
-//     }
-
-//   }
     url = '../../assets/house-imgs (7).png';
   onSelectFile(event:any) {
     if (event.target.files && event.target.files[0]) {
